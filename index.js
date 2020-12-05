b0e5e4c9557eba4157d12abe6ae455fb7b37957d
ManivishalBurra <72870001+ManivishalBurra@users.noreply.github.com>
2020-12-06 01:54:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
-$(window).on("beforeunload",function(){$(window).scrollTop(0)},{passive:!0}),window.addEventListener("scroll",function(){const e=window.scrollY;console.log(e),e>=50?($(".navlinks").css("color","black"),document.querySelector("nav").classList.add("backdrop-blur"),document.querySelectorAll("img")[0].classList.add("imagext"),document.querySelectorAll("img")[1].classList.add("imagetext"),document.querySelectorAll("ul")[1].classList.add("symba"),document.querySelectorAll("a")[3].classList.add("symbal"),document.querySelectorAll("a")[4].classList.add("symbal"),document.querySelectorAll("a")[3].innerHTML='<i class="fas fa-phone-alt"></i>',document.querySelectorAll("a")[4].innerHTML='<i class="far fa-envelope"></i>'):(document.querySelector("nav").classList.remove("backdrop-blur"),document.querySelectorAll("img")[0].classList.remove("imagext"),document.querySelectorAll("img")[1].classList.remove("imagetext"),document.querySelectorAll("a")[3].innerHTML='<i class="fas fa-phone-alt"></i> +91-7993538436',document.querySelectorAll("a")[4].innerHTML='<i class="far fa-envelope"></i> averxdrugs@gmail.com',document.querySelectorAll("ul")[1].classList.remove("symba"),document.querySelectorAll("a")[3].classList.remove("symbal"),document.querySelectorAll("a")[4].classList.remove("symbal"),document.querySelector("div").classList.remove("color"),$(".navlinks").css("color","black"))},{passive:!0});var lastScrollTop=0;navbar=document.getElementById("navbar"),window.addEventListener("scroll",function(){var e=window.pageYOffset||document.documentElement.scrollTop;navbar.style.top=e>lastScrollTop&&e>2310?"-90px":"0",lastScrollTop=e},{passive:!0});const parallax=document.getElementById("parallax");window.addEventListener("scroll",function(){var e=window.pageYOffset,l=window.innerWidth;e>=3260&&e<=4560&&l>=600?(e-=3260,parallax.style.backgroundPositionY="-"+.1*e+"px"):l>=600?(console.log("intViewportWidth: "+l),video.style.backgroundPositionY="-"+.1*e+"px",e-=792,slide2.style.backgroundPositionY="-"+.1*e+"px",e-=792,slide3.style.backgroundPositionY="-"+.1*e+"px",console.log("newoffset: "+e)):video.style.backgroundPositionY="-"+0*e+"px"},{passive:!0});
+(function () {
+  var toggle = document.querySelector('.nav-toggle');
+
+  toggle.addEventListener('click', function(e) {
+    this.classList.toggle('opened');
+  });
+})();
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+},{passive:true});
+
+
+window.addEventListener("scroll",function(){
+  const scrolled= window.scrollY;
+  console.log(scrolled);
+  if(scrolled>=50)
+  {
+   $(".navlinks").css("color","black");
+   document.querySelector("nav").classList.add("backdrop-blur");
+   document.querySelectorAll("img")[0].classList.add("imagext");
+   document.querySelectorAll("img")[1].classList.add("imagetext");
+   document.querySelectorAll("ul")[1].classList.add("symba");
+   document.querySelectorAll("a")[3].classList.add("symbal");
+   document.querySelectorAll("a")[4].classList.add("symbal");
+   document.querySelectorAll("a")[3].innerHTML='<i class="fas fa-phone-alt"></i>';
+   document.querySelectorAll("a")[4].innerHTML='<i class="far fa-envelope"></i>';
+  }
+  else
+  {document.querySelector("nav").classList.remove("backdrop-blur");
+   document.querySelectorAll("img")[0].classList.remove("imagext");
+   document.querySelectorAll("img")[1].classList.remove("imagetext");
+   document.querySelectorAll("a")[3].innerHTML='<i class="fas fa-phone-alt"></i> +91-7993538436';
+   document.querySelectorAll("a")[4].innerHTML='<i class="far fa-envelope"></i> averxdrugs@gmail.com';
+   document.querySelectorAll("ul")[1].classList.remove("symba");
+   document.querySelectorAll("a")[3].classList.remove("symbal");
+   document.querySelectorAll("a")[4].classList.remove("symbal");
+   document.querySelector("div").classList.remove("color");
+   $(".navlinks").css("color","black");
+ }
+},{passive:true});
+//this is for the closing of nav bar
+var lastScrollTop=0;
+navbar=document.getElementById("navbar");
+window.addEventListener("scroll",function(){
+var scrollTop=window.pageYOffset || document.documentElement.scrollTop;
+if(scrollTop>lastScrollTop && scrollTop>2310)
+{
+navbar.style.top="-90px";
+}
+else
+{
+navbar.style.top="0";
+}
+lastScrollTop=scrollTop;
+},{passive:true});
+//this is for poping of averxDrugs title;
+
+//this is for parallax effect;
+const parallax=document.getElementById("parallax");
+window.addEventListener("scroll",function(){
+  var offset=window.pageYOffset;
+
+  var intViewportWidth = window.innerWidth;
+  var x=792;
+
+
+ if(offset>=3260 && offset<=4560 &&intViewportWidth>=600)
+ {
+  offset=offset-3260;
+  parallax.style.backgroundPositionY="-"+offset*0.1+"px";
+ }
+ else if(intViewportWidth>=600)
+ {  console.log('intViewportWidth: '+intViewportWidth);
+    video.style.backgroundPositionY="-"+offset*0.1+"px";
+    offset=offset-x;
+    slide2.style.backgroundPositionY="-"+offset*0.1+"px";
+    offset=offset-x;
+    slide3.style.backgroundPositionY="-"+offset*0.1+"px";
+    console.log('newoffset: '+ offset);
+ }
+ else
+ {
+   video.style.backgroundPositionY="-"+offset*0+"px";
+ }
+},{passive:true});
